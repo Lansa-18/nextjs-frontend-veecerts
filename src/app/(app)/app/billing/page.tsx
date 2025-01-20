07d1d8fa2e5a0e3fbf55ca5097ca462afd7da9ad
Mapelujo <olamideiyanda18@gmail.com>
2025-01-20 19:58:19 +0100

    progress on the billing page.

--- src/app/(app)/app/billing/page.tsx
+++ src/app/(app)/app/billing/page.tsx
@@ -1,11 +1,35 @@
-import SubscriptionPackages from "@/components/molecules/m-subscription-packages";
+// import SubscriptionPackages from "@/components/molecules/m-subscription-packages";
 import { H2 } from "@/components/ui/typography";
+import Image from "next/image";
 
 const BillingPage = () => {
   return (
-    <div className="flex flex-col p-2 gap-4">
+    <div className="flex flex-col p-4 gap-4 bg-[#f2f5f9] min-h-screen">
       <H2>Billing</H2>
-      <SubscriptionPackages />
+      {/* <SubscriptionPackages /> */}
+      <main>
+        <article>
+          <div>
+            <Image
+              src="/Credit Card.png"
+              alt="credit-card"
+              width={450}
+              height={240}
+            />
+          </div>
+        </article>
+        <article className="font-poppins text-[14px] font-bold leading-[19.6px] bg-white rounded-[1.5px] mt-[18px] p-[22px]">
+          <div className="flex justify-between items-center">
+            <h2>Payment Method</h2>
+            <button className="uppercase px-[14px] rounded-[13px] py-[10px] bg-1 flex justify-center items-center text-center text-white text-[10px]">
+              Add a new card
+            </button>
+          </div>
+          <div>
+
+          </div>
+        </article>
+      </main>
     </div>
   );
 };
